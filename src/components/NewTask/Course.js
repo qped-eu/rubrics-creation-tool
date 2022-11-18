86bb7384710fb1cf09b3ac373b0ecb93ece1b4a4
0nlineSam <uni@sam.tadjiky.de>
2022-11-18 12:15:45 +0100

    Implemented Course Component

--- src/components/NewTask/Course.js
+++ src/components/NewTask/Course.js
@@ -1,19 +1,38 @@
-import React from "react";
+import { MenuItem, Select, Typography } from "@mui/material";
+import _ from "lodash";
+import React, { useState } from "react";
 
 function Course() {
+  const options = [
+    "NHL - Java 1",
+    "NHL - Python",
+    "OUNL - IB0902",
+    "OUNL – IB1502",
+    "UMR - OOP",
+    "UOC - DS",
+    "TU/e - 2IP90",
+    "TU/e - 2IPC0",
+  ];
+
+  const [value, setValue] = useState(options[0]);
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <>
-      <h2>Course</h2>
-      <select id="course_text">
-        <option value="nhl_java1">NHL - Java 1</option>
-        <option value="nhl_python">NHL - Python</option>
-        <option value="ounl_ib0902">OUNL - IB0902</option>
-        <option value="ounl_ib1502">OUNL – IB1502</option>
-        <option value="umr_oop">UMR - OOP</option>
-        <option value="uoc_ds">UOC - DS</option>
-        <option value="tue_2ip09">TU/e - 2IP90</option>
-        <option value="tue_2ipc0">TU/e - 2IPC0</option>
-      </select>
+      <Typography variant="h4">Course</Typography>
+      <Select
+        value={value}
+        sx={{ mt: 2 }}
+        inputProps={{ "aria-label": "Without label" }}
+        onChange={handleChange}
+      >
+        {_.map(options, (option) => (
+          <MenuItem value={option}>{option}</MenuItem>
+        ))}
+      </Select>
     </>
   );
 }
