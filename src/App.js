ff85eeb1a9ae86fe4a5b738ae2ff30ab9c6cfe97
0nlineSam <uni@sam.tadjiky.de>
2022-12-23 23:58:48 +0100

    Code formatting

--- src/App.js
+++ src/App.js
@@ -5,66 +5,55 @@ import { TaskSelector, TaskUpload } from "./components/TaskManager";
 import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
 
-
-const theme= createTheme({
+const theme = createTheme({
   palette: {
-    type: 'dark',
+    type: "dark",
     primary: {
-      main: '#485B8F',
+      main: "#485B8F",
     },
     secondary: {
-      main: '#d45113',
+      main: "#d45113",
     },
     success: {
-      main: '#8CE789',
+      main: "#8CE789",
     },
   },
-
-
-})
-
-
-
-
+});
 
 function App() {
   const [error, setError] = useState(null);
   return (
     <ThemeProvider theme={theme}>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      
-        <Typography>QPED Rubric Creation Tool</Typography>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
 
-        <Stack spacing={1}>
-          
-          <TaskUpload setError={setError} />
-          <Button
-            variant="contained"
-            href="new_task"
-            size="large"
-            disableElevation
-            color="secondary"
-           // style={{border :'2px solid'}}
-            sx={{height:50}}
-            
-            
+          <Typography>QPED Rubric Creation Tool</Typography>
 
-          >
-            {" "}
-            create new Task
-          </Button>
-          <TaskSelector />
-        </Stack>
-      </header>
-      <Snackbar
-        open={!!error}
-        autoHideDuration={6000}
-        onClose={() => setError(null)}
-        message={error}
-      />
-    </div>
+          <Stack spacing={1}>
+            <TaskUpload setError={setError} />
+            <Button
+              variant="contained"
+              href="new_task"
+              size="large"
+              disableElevation
+              color="secondary"
+              // style={{border :'2px solid'}}
+              sx={{ height: 50 }}
+            >
+              {" "}
+              create new Task
+            </Button>
+            <TaskSelector />
+          </Stack>
+        </header>
+        <Snackbar
+          open={!!error}
+          autoHideDuration={6000}
+          onClose={() => setError(null)}
+          message={error}
+        />
+      </div>
     </ThemeProvider>
   );
 }
